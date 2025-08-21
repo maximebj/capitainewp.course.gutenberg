6ca9699a79e34373692c2d02bb024ccb9fb7e1cd
Maxime BJ <maxime@dysign.fr>
2025-08-21 13:24:49 +0200

    removed useless fragment

--- src/18-table-of-contents/edit.js
+++ src/18-table-of-contents/edit.js
@@ -4,7 +4,7 @@ import {
 	RichText,
 } from "@wordpress/block-editor";
 import { useSelect, useDispatch } from "@wordpress/data";
-import { Fragment, useEffect } from "@wordpress/element";
+import { useEffect } from "@wordpress/element";
 
 import {
 	getHeadingsFromContent,
